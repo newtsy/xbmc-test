37ec51278e82422f6d5fabec7e0378bf984b60f9
Lars Op den Kamp <lars@opdenkamp.eu>
2012-03-28 21:38:31 +0200

    Revert "squashed PR570". Re-pull from mainline
    This reverts commit 37d89f72a876e55219b751c30897957b2c6e1370.

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2645,26 +2645,6 @@ bool CApplication::OnAction(const CAction &action)
   // Check for global volume control
   if (action.GetAmount() && (action.GetID() == ACTION_VOLUME_UP || action.GetID() == ACTION_VOLUME_DOWN))
   {
-    /* try to set the volume on a connected amp */
-  #ifdef HAVE_LIBCEC
-    vector<CPeripheral *> peripherals;
-    if (g_peripherals.GetPeripheralsWithFeature(peripherals, FEATURE_CEC))
-    {
-      for (unsigned int iPeripheralPtr = 0; iPeripheralPtr < peripherals.size(); iPeripheralPtr++)
-      {
-        CPeripheralCecAdapter *cecDevice = (CPeripheralCecAdapter *) peripherals.at(iPeripheralPtr);
-        if (cecDevice && cecDevice->HasConnectedAudioSystem())
-        {
-          if (action.GetID() == ACTION_VOLUME_UP)
-            cecDevice->ScheduleVolumeUp();
-          else
-            cecDevice->ScheduleVolumeDown();
-          return true;
-        }
-      }
-    }
-  #endif
-
     if (!m_pPlayer || !m_pPlayer->IsPassthrough())
     {
       // increase or decrease the volume
@@ -3385,8 +3365,7 @@ void CApplication::Stop(int exitCode)
 {
   try
   {
-    CVariant vExitCode(exitCode);
-    CAnnouncementManager::Announce(System, "xbmc", "OnQuit", vExitCode);
+    CAnnouncementManager::Announce(System, "xbmc", "OnQuit");
 
     // cancel any jobs from the jobmanager
     CJobManager::GetInstance().CancelJobs();
@@ -5144,49 +5123,11 @@ void CApplication::ShowVolumeBar(const CAction *action)
 
 bool CApplication::IsMuted() const
 {
-  /* try to set the mute setting on a connected amp */
-#ifdef HAVE_LIBCEC
-  vector<CPeripheral *> peripherals;
-  if (g_peripherals.GetPeripheralsWithFeature(peripherals, FEATURE_CEC))
-  {
-    for (unsigned int iPeripheralPtr = 0; iPeripheralPtr < peripherals.size(); iPeripheralPtr++)
-    {
-      CPeripheralCecAdapter *cecDevice = (CPeripheralCecAdapter *) peripherals.at(iPeripheralPtr);
-      if (cecDevice && cecDevice->HasConnectedAudioSystem())
-        return false;
-    }
-  }
-#endif
   return g_settings.m_bMute;
-}
-
-bool CApplication::CecMute(void)
-{
-  /* try to set the mute setting on a connected amp */
-#ifdef HAVE_LIBCEC
-  vector<CPeripheral *> peripherals;
-  if (g_peripherals.GetPeripheralsWithFeature(peripherals, FEATURE_CEC))
-  {
-    for (unsigned int iPeripheralPtr = 0; iPeripheralPtr < peripherals.size(); iPeripheralPtr++)
-    {
-      CPeripheralCecAdapter *cecDevice = (CPeripheralCecAdapter *) peripherals.at(iPeripheralPtr);
-      if (cecDevice && cecDevice->HasConnectedAudioSystem())
-      {
-        cecDevice->ScheduleMute();
-        return true;
-      }
-    }
-  }
-#endif
-
-  return false;
 }
 
 void CApplication::ToggleMute(void)
 {
-  if (CecMute())
-    return;
-
   if (g_settings.m_bMute)
     UnMute();
   else
@@ -5195,9 +5136,6 @@ void CApplication::ToggleMute(void)
 
 void CApplication::Mute()
 {
-  if (CecMute())
-    return;
-
   g_settings.m_iPreMuteVolumeLevel = GetVolume();
   SetVolume(0);
   g_settings.m_bMute = true;
@@ -5205,9 +5143,6 @@ void CApplication::Mute()
 
 void CApplication::UnMute()
 {
-  if (CecMute())
-    return;
-
   SetVolume(g_settings.m_iPreMuteVolumeLevel);
   g_settings.m_iPreMuteVolumeLevel = 0;
   g_settings.m_bMute = false;
