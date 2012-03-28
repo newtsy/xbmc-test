60d65a6353e47085d69b0aa95e220b77f0f20659
Martin Ellis <malard@gmail.com>
2012-03-28 14:48:16 +0100

    added: teamcitybuild batch file to handle packaging

---
+++ project/Win32BuildSetup/TeamCityBuild.bat
@@ -0,0 +1,146 @@
+SET comp=vs2010
+SET target=dx
+SET buildmode=ask
+SET promptlevel=prompt
+SET buildmingwlibs=true
+SET exitcode=0
+FOR %%b in (%1, %2, %3, %4, %5) DO (
+	IF %%b==vs2010 SET comp=vs2010
+	IF %%b==dx SET target=dx
+	IF %%b==gl SET target=gl
+	IF %%b==clean SET buildmode=clean
+	IF %%b==noclean SET buildmode=noclean
+	IF %%b==noprompt SET promptlevel=noprompt
+	IF %%b==nomingwlibs SET buildmingwlibs=false
+)
+
+SET buildconfig=Release (DirectX)
+set EXE= "..\VS2010Express\XBMC\%buildconfig%\XBMC.exe"
+
+  IF %buildmingwlibs%==true (
+    ECHO Compiling mingw libs
+    ECHO bla>noprompt
+    IF EXIST errormingw del errormingw > NUL
+	IF %buildmode%==clean (
+	  ECHO bla>makeclean
+	)
+    call buildmingwlibs.bat
+    IF EXIST errormingw (
+    	set DIETEXT="failed to build mingw libs"
+    	goto DIE
+    )
+  )
+  
+  ECHO Copying files...
+  IF EXIST BUILD_WIN32 rmdir BUILD_WIN32 /S /Q
+
+  Echo .svn>exclude.txt
+  Echo CVS>>exclude.txt
+  Echo .so>>exclude.txt
+  Echo Thumbs.db>>exclude.txt
+  Echo Desktop.ini>>exclude.txt
+  Echo dsstdfx.bin>>exclude.txt
+  Echo exclude.txt>>exclude.txt
+  rem and exclude potential leftovers
+  Echo mediasources.xml>>exclude.txt
+  Echo advancedsettings.xml>>exclude.txt
+  Echo guisettings.xml>>exclude.txt
+  Echo profiles.xml>>exclude.txt
+  Echo sources.xml>>exclude.txt
+  Echo userdata\cache\>>exclude.txt
+  Echo userdata\database\>>exclude.txt
+  Echo userdata\playlists\>>exclude.txt
+  Echo userdata\script_data\>>exclude.txt
+  Echo userdata\thumbnails\>>exclude.txt
+  rem UserData\visualisations contains currently only xbox visualisationfiles
+  Echo userdata\visualisations\>>exclude.txt
+  rem other platform stuff
+  Echo lib-osx>>exclude.txt
+  Echo players\mplayer>>exclude.txt
+  Echo FileZilla Server.xml>>exclude.txt
+  Echo asound.conf>>exclude.txt
+  Echo voicemasks.xml>>exclude.txt
+  Echo Lircmap.xml>>exclude.txt
+  
+  md BUILD_WIN32\Xbmc
+
+  xcopy %EXE% BUILD_WIN32\Xbmc > NUL
+  xcopy ..\..\userdata BUILD_WIN32\Xbmc\userdata /E /Q /I /Y /EXCLUDE:exclude.txt > NUL
+  copy ..\..\copying.txt BUILD_WIN32\Xbmc > NUL
+  copy ..\..\LICENSE.GPL BUILD_WIN32\Xbmc > NUL
+  copy ..\..\known_issues.txt BUILD_WIN32\Xbmc > NUL
+  xcopy dependencies\*.* BUILD_WIN32\Xbmc /Q /I /Y /EXCLUDE:exclude.txt  > NUL
+  copy sources.xml BUILD_WIN32\Xbmc\userdata > NUL
+  
+  xcopy ..\..\language BUILD_WIN32\Xbmc\language /E /Q /I /Y /EXCLUDE:exclude.txt  > NUL
+  xcopy ..\..\addons BUILD_WIN32\Xbmc\addons /E /Q /I /Y /EXCLUDE:exclude.txt > NUL
+  xcopy ..\..\system BUILD_WIN32\Xbmc\system /E /Q /I /Y /EXCLUDE:exclude.txt  > NUL
+  xcopy ..\..\media BUILD_WIN32\Xbmc\media /E /Q /I /Y /EXCLUDE:exclude.txt  > NUL
+  xcopy ..\..\sounds BUILD_WIN32\Xbmc\sounds /E /Q /I /Y /EXCLUDE:exclude.txt  > NUL
+    
+  IF EXIST error.log del error.log > NUL
+  SET build_path=%CD%
+  ECHO ------------------------------------------------------------
+  ECHO Building Confluence Skin...
+  cd ..\..\addons\skin.confluence
+  call build.bat > NUL
+  cd %build_path%
+
+  IF EXIST exclude.txt del exclude.txt  > NUL
+  ECHO ------------------------------------------------------------
+  ECHO Build Succeeded!
+ 
+  ECHO ------------------------------------------------------------
+  ECHO Generating installer includes...
+  call genNsisIncludes.bat
+  ECHO ------------------------------------------------------------
+  call getdeploydependencies.bat
+  SET GIT_REV=%build.number%
+  SET XBMC_SETUPFILE=XBMCSetup-%GIT_REV%-%target%.exe
+  ECHO Creating installer %XBMC_SETUPFILE%...
+  IF EXIST %XBMC_SETUPFILE% del %XBMC_SETUPFILE% > NUL
+  rem get path to makensis.exe from registry, first try tab delim
+  FOR /F "tokens=2* delims=	" %%A IN ('REG QUERY "HKLM\Software\NSIS" /ve') DO SET NSISExePath=%%B
+
+  IF NOT EXIST "%NSISExePath%" (
+    rem try with space delim instead of tab
+    FOR /F "tokens=2* delims= " %%A IN ('REG QUERY "HKLM\Software\NSIS" /ve') DO SET NSISExePath=%%B
+  )
+      
+  IF NOT EXIST "%NSISExePath%" (
+    rem fails on localized windows (Default) becomes (Par Défaut)
+    FOR /F "tokens=3* delims=	" %%A IN ('REG QUERY "HKLM\Software\NSIS" /ve') DO SET NSISExePath=%%B
+  )
+
+  IF NOT EXIST "%NSISExePath%" (
+    FOR /F "tokens=3* delims= " %%A IN ('REG QUERY "HKLM\Software\NSIS" /ve') DO SET NSISExePath=%%B
+  )
+  
+  rem proper x64 registry checks
+  IF NOT EXIST "%NSISExePath%" (
+    ECHO using x64 registry entries
+    FOR /F "tokens=2* delims=	" %%A IN ('REG QUERY "HKLM\Software\Wow6432Node\NSIS" /ve') DO SET NSISExePath=%%B
+  )
+  IF NOT EXIST "%NSISExePath%" (
+    rem try with space delim instead of tab
+    FOR /F "tokens=2* delims= " %%A IN ('REG QUERY "HKLM\Software\Wow6432Node\NSIS" /ve') DO SET NSISExePath=%%B
+  )
+  IF NOT EXIST "%NSISExePath%" (
+    rem on win 7 x64, the previous fails
+    FOR /F "tokens=3* delims=	" %%A IN ('REG QUERY "HKLM\Software\Wow6432Node\NSIS" /ve') DO SET NSISExePath=%%B
+  )
+  IF NOT EXIST "%NSISExePath%" (
+    rem try with space delim instead of tab
+    FOR /F "tokens=3* delims= " %%A IN ('REG QUERY "HKLM\Software\Wow6432Node\NSIS" /ve') DO SET NSISExePath=%%B
+  )
+
+  SET NSISExe=%NSISExePath%\makensis.exe
+  "%NSISExe%" /V1 /X"SetCompressor /FINAL lzma" /Dxbmc_root="%CD%\BUILD_WIN32" /Dxbmc_revision="%GIT_REV%" /Dxbmc_target="%target%" "XBMC for Windows.nsi"
+  IF NOT EXIST "%XBMC_SETUPFILE%" (
+	  set DIETEXT=Failed to create %XBMC_SETUPFILE%. NSIS installed?
+	  goto DIE
+  )
+  ECHO ------------------------------------------------------------
+  ECHO Done!
+  ECHO Setup is located at %CD%\%XBMC_SETUPFILE%
+  ECHO ------------------------------------------------------------
